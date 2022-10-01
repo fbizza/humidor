e8485f5a4b9ab02cd09c091543201093ccdf867c
fbizza <109001290+fbizza@users.noreply.github.com>
2022-10-01 17:43:57 +0200

    Temperature and humidity reading

--- code.ino
+++ code.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#define DHTPIN 5
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial.print("Temperature: "); Serial.println(t);
+  Serial.print("Humidity: "); Serial.println(h);
+  delay(5000);
 }
